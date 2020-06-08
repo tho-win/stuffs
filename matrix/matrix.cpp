0e746989f79c61afe0cdffcd30a1424da3a527d7
tho-win <tnn7yc@virginia.edu>
2020-06-08 12:21:53 -0500

    added add, substract and multiply

--- matrix/matrix.cpp
+++ matrix/matrix.cpp
@@ -32,7 +32,7 @@ void DenseMatrix::set(int row, int column, int value) {
 }
 
 void DenseMatrix::resize(int newRow, int newColumn) {
-    newMatrix = new double*[rows];
+    double *newMatrix = new double*[rows];
     for (int i=0; i<newRow; i++){
         newMatrix[i] = new double[newColumn];
     }
@@ -46,4 +46,52 @@ void DenseMatrix::resize(int newRow, int newColumn) {
     rows = newRow;
     cols = newColumn;
     matrix = newMatrix; //need to look at this again cause old matrix might not be deallocate here
+}
+
+Matrix DenseMatrix::add(const Matrix& other) {
+    double *newMatrix = new double*[rows];
+    for (int i=0; i<rows; i++){
+        newMatrix[i] = new double[cols];
+    }
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            newMatrix[i][j] = matrix[i][j] + other->matrix[i][j];
+        }
+    }
+    return newMatrix;
+}
+
+Matrix DenseMatrix::subtract(const Matrix& other) {
+    double *newMatrix = new double*[rows];
+    for (int i=0; i<rows; i++){
+        newMatrix[i] = new double[cols];
+    }
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            newMatrix[i][j] = matrix[i][j] - other->matrix[i][j];
+        }
+    }
+    return newMatrix;
+}
+
+Matrix DenseMatrix::multiply(const Matrix& other) {
+    //no error catching assuming matrices are multipliable
+    double *newMatrix = new double*[rows];
+    for (int i=0; i<rows; i++){
+        newMatrix[i] = new double[other->cols];
+    }
+    for (int i = 0; i < rows; ++i) {
+        double r[] = matrix[i];
+        double c[cols];
+        for (int j = 0; j < cols; ++j) {
+            c[i] = other->matrix[i][j];
+        }
+        double temp = 0;
+        for (int k=0; k < rows; k++) {
+            temp += r[k]*c[k];
+        }
+        newMatrix[i][j] = temp;
+        }
+    }
+    return newMatrix;
 }
